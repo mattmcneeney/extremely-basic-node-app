78c0e1feb40e2c2c31846228a13a08f3befb2283
Matt McNeeney <mmcneeney@pivotal.io>
2017-10-04 14:57:40 +0100

    add cfenv and print appenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
 require('express')().listen(process.env.PORT || 3000, function() {
    console.log('extremely-basic-node-app running');
+   console.log('check out my env:');
+   console.log(require('cfenv').getAppEnv());
 });
