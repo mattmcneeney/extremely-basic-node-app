9279b6226e5eec1b413933210b2b2efd3174e779
Matt McNeeney <mmcneeney@pivotal.io>
2018-01-15 11:59:50 +0000

    update node version

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ require('express')().listen(process.env.PORT || 3000, function() {
    console.log('check out my env:');
    console.log(JSON.stringify(require('cfenv').getAppEnv(), null, 2));
 });
+
