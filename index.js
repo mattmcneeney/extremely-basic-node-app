9d47d050ed9fef19e813c3f1202bf59d740e22a9
Matt McNeeney <mmcneeney@pivotal.io>
2018-05-02 16:27:16 +0100

    remove manifest

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('express')().listen(process.env.PORT || 3000, function() {
-   console.log('extremely-basic-node-app running');
-   console.log('check out my env:');
+   console.log('We have liftoff!');
+   console.log('Check out my env:');
    console.log(JSON.stringify(require('cfenv').getAppEnv(), null, 2));
 });
 
